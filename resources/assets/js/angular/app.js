8b07f258a0d973526f3838391ec449a01c76fae6
LukaVerhoeven <lka.v.lv@gmail.com>
2017-05-17 16:19:29 +0200

    c9

--- resources/assets/js/angular/app.js
+++ resources/assets/js/angular/app.js
@@ -2,4 +2,3 @@ var app = angular.module('bubble',['ngSanitize'])
         // .constant('API_URL','http://bubble.local/api/');
         // .constant('API_URL','http://lukaverhoevenmtantwerpeu.webhosting.be/api/');
         .constant('API_URL','http://bubble-lukaverhoeven.c9users.io/api/');
-        .constant('a','aa');
