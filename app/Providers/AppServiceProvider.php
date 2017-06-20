7c7117c1f62c3c01f11df3ca12e890d8c96e67b4
LukaVerhoeven <lka.v.lv@gmail.com>
2017-06-20 11:07:53 +0200

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        URL::forceScheme('https'); //force https for laravel 5.4
+        // URL::forceScheme('https'); //force https for laravel 5.4
     }
 
     /**
