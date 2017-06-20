f8fa6d5a7641ad1bebc436ca6a353f2effd947d4
LukaVerhoeven <lka.v.lv@gmail.com>
2017-06-20 10:56:19 +0200

    forcessl

--- routes/web.php
+++ routes/web.php
@@ -16,51 +16,53 @@
 // Route::group(['middleware' => 'auth'], function () {
 // 	Route::get('/api/message/{id?}','MessageController@index')->middleware('auth');
 // });
+Route::group(['middleware' => ['auth','forceSSL']], function()
+{
+	Route::get('/', 'ChatController@index');
+	Route::get('/api/getChatRooms', 'ChatController@getChatRooms');
+	Route::post('/api/username', 'ChatController@editUserName')->middleware('auth');
+	Route::post('/api/email', 'ChatController@editUserEmail')->middleware('auth');
 
-Route::get('/', 'ChatController@index');
-Route::get('/api/getChatRooms', 'ChatController@getChatRooms');
-Route::post('/api/username', 'ChatController@editUserName')->middleware('auth');
-Route::post('/api/email', 'ChatController@editUserEmail')->middleware('auth');
+	// messages
+	Route::get('/api/message/{id?}', 'MessageController@index')->middleware('auth');
+	Route::post('/api/message', 'MessageController@store');
+	Route::post('/api/getThemes/{id}', 'MessageController@getThemes');
+	Route::post('/api/newMessage', 'MessageController@unread');
+	Route::post('/api/readMessages', 'MessageController@read');
+	Route::get('/api/getMessages/{id?}', 'MessageController@getMessages');
 
-// messages
-Route::get('/api/message/{id?}', 'MessageController@index')->middleware('auth');
-Route::post('/api/message', 'MessageController@store');
-Route::post('/api/getThemes/{id}', 'MessageController@getThemes');
-Route::post('/api/newMessage', 'MessageController@unread');
-Route::post('/api/readMessages', 'MessageController@read');
-Route::get('/api/getMessages/{id?}', 'MessageController@getMessages');
+	// friends
+	Route::get('/api/searchNewFriend/{letters?}', 'FriendController@searchNewFriend')->middleware('auth');
+	Route::get('/api/friends', 'FriendController@getFriendList');
+	Route::post('/api/addFriend', 'FriendController@addFriend')->middleware('auth');
+	Route::post('/api/declineFriend', 'FriendController@decline')->middleware('auth');
+	Route::post('/api/deleteFriend', 'FriendController@delete')->middleware('auth');
+	Route::get('/api/friendRequests', 'FriendController@getFriendRequests')->middleware('auth');
+	Route::get('/api/searchFriend/{id?}', 'FriendController@searchFriend');
+	Route::post('/api/onlineState', 'FriendController@sendOnline');
+	Route::post('/api/onlineAnswer', 'FriendController@receiveOnline');
+	Route::post('/api/sendLogout', 'FriendController@sendOffline');
+	Route::post('/api/renameFriend', 'FriendController@renameFriend');
 
-// friends
-Route::get('/api/searchNewFriend/{letters?}', 'FriendController@searchNewFriend')->middleware('auth');
-Route::get('/api/friends', 'FriendController@getFriendList');
-Route::post('/api/addFriend', 'FriendController@addFriend')->middleware('auth');
-Route::post('/api/declineFriend', 'FriendController@decline')->middleware('auth');
-Route::post('/api/deleteFriend', 'FriendController@delete')->middleware('auth');
-Route::get('/api/friendRequests', 'FriendController@getFriendRequests')->middleware('auth');
-Route::get('/api/searchFriend/{id?}', 'FriendController@searchFriend');
-Route::post('/api/onlineState', 'FriendController@sendOnline');
-Route::post('/api/onlineAnswer', 'FriendController@receiveOnline');
-Route::post('/api/sendLogout', 'FriendController@sendOffline');
-Route::post('/api/renameFriend', 'FriendController@renameFriend');
+	//groups
+	Route::post('/api/createGroup', 'GroupController@createGroup')->middleware('auth');
+	Route::post('/api/accept', 'GroupController@accept')->middleware('auth');
+	Route::post('/api/decline', 'GroupController@decline')->middleware('auth');
+	Route::post('/api/addFriendToGroup', 'GroupController@addFriendToGroup');
+	Route::post('/api/toggleAdmin', 'GroupController@toggleAdmin');
+	Route::post('/api/deleteGroup', 'GroupController@delete');
+	Route::post('/api/renameChat', 'GroupController@renameChat');
+	// theme 
+	Route::post('/api/NewTheme', 'ThemeController@create');
+	Route::post('/api/updateTheme', 'ThemeController@update');
+	Route::post('/api/toggleTheme', 'ThemeController@toggle');
+	Route::post('/api/deleteTheme', 'ThemeController@delete');
 
-//groups
-Route::post('/api/createGroup', 'GroupController@createGroup')->middleware('auth');
-Route::post('/api/accept', 'GroupController@accept')->middleware('auth');
-Route::post('/api/decline', 'GroupController@decline')->middleware('auth');
-Route::post('/api/addFriendToGroup', 'GroupController@addFriendToGroup');
-Route::post('/api/toggleAdmin', 'GroupController@toggleAdmin');
-Route::post('/api/deleteGroup', 'GroupController@delete');
-Route::post('/api/renameChat', 'GroupController@renameChat');
-// theme 
-Route::post('/api/NewTheme', 'ThemeController@create');
-Route::post('/api/updateTheme', 'ThemeController@update');
-Route::post('/api/toggleTheme', 'ThemeController@toggle');
-Route::post('/api/deleteTheme', 'ThemeController@delete');
+	// User
+	Auth::routes();
+	Route::post('/api/profileImage', 'ChatController@profileImage')->middleware('auth');
 
-// User
-Auth::routes();
-Route::post('/api/profileImage', 'ChatController@profileImage')->middleware('auth');
+	// Route::get('logout','auth\LoginController@logout');
 
-// Route::get('logout','auth\LoginController@logout');
-
-Route::get('/home', 'HomeController@index');
+	Route::get('/home', 'HomeController@index');
+});
